2806b439082c98446a55a9fc9115efeb47ddf74c
Marina <marinamcsouza@gmail.com>
2024-11-20 19:44:27 -0300

    feat: atualiza game

--- Tetris/GameWindow.java
+++ Tetris/GameWindow.java
@@ -5,24 +5,21 @@ import javax.swing.JFrame;
 
 public class GameWindow extends JFrame {
 
-	// é um número sem sentido para suprimir um alerta desnecessário.
-	private static final long serialVersionUID = 8737819995837542594L;
+    private static final long serialVersionUID = 8737819995837542594L;
 
-	public GameWindow() {
-		setTitle("Tetris :D");
-		setSize(400, 814);
-		setResizable(false);
+    public GameWindow() {
+        setTitle("Tetris :D");
+        setSize(400, 700); // Diminuir altura para 700
+        setResizable(false);
 
-		setLayout(new GridLayout(1, 2));
+        setLayout(new GridLayout(1, 2));
 
-		// você pode ajustar a resolução do temporizador aqui, mas este é o valor ideal para este jogo.
-		GameBoardPanel gameBoard = new GameBoardPanel(this, 400);
-		add(gameBoard);
-		gameBoard.start();
+        GameBoardPanel gameBoard = new GameBoardPanel(this, 400);
+        add(gameBoard);
+        gameBoard.start();
 
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setLocationRelativeTo(null);
-		setVisible(true);
-	}
-
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+        setVisible(true);
+    }
 }
